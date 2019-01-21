598e278a65f64f8db4ca77d200d12cb115dee59f
Ahmad <ahmad.alhourani@xlabtechs.com>
2019-01-21 11:17:11 +0200

    Category

---
+++ resources/views/backend/categories/table.blade.php
@@ -0,0 +1,23 @@
+<div class="table-responsive">
+    <table class="table">
+        <thead>
+        <tr>
+                <th>{{ __('labels.backend.categories.table.name') }}</th>
+                
+            <th>{{ __('labels.general.actions') }}</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach ($categories as $category)
+        <tr>
+             
+                <td>{{  $category->name }}</td>  
+                
+
+               <td>{!! $category->action_buttons !!}</td>
+        </tr>
+
+        @endforeach
+        </tbody>
+    </table>
+</div>
