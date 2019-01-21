a30e0800527addc41964cab8f4e6d206874036b9
Ahmad <ahmad.alhourani@xlabtechs.com>
2019-01-21 11:16:31 +0200

    Person

---
+++ resources/views/backend/men/table.blade.php
@@ -0,0 +1,27 @@
+<div class="table-responsive">
+    <table class="table">
+        <thead>
+        <tr>
+                <th>{{ __('labels.backend.men.table.name') }}</th>
+                <th>{{ __('labels.backend.men.table.email') }}</th>
+                <th>{{ __('labels.backend.men.table.sms') }}</th>
+                
+            <th>{{ __('labels.general.actions') }}</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach ($men as $man)
+        <tr>
+             
+                <td>{{  $man->name }}</td>  
+                <td>{{  $man->email }}</td>  
+                <td>{{  $man->sms }}</td>  
+                
+
+               <td>{!! $man->action_buttons !!}</td>
+        </tr>
+
+        @endforeach
+        </tbody>
+    </table>
+</div>
